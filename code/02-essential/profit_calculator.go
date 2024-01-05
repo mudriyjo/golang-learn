d791c20a8c21b134e20a1c7f729b45306adef8c0
Alexander Babin <alexander_babin@mckinsey.com>
2024-01-05 14:39:04 +0700

    finish excercise with rofict calculator. Add saving to file and validation functionality

--- code/02-essential/profit_calculator.go
+++ code/02-essential/profit_calculator.go
@@ -1,6 +1,12 @@
 package main
 
 import "fmt"
+import "os"
+
+func main() {
+	revenuCalculator()
+	bank()
+}
 
 /* 
  Practices exercise
@@ -8,22 +14,38 @@ import "fmt"
  Calculate
  Earn before tax, Earn after tax (profit) and ratio = Earn before tax / profit
  Print all result to console
+ + Validate user input
+ - non negative
+ - not 0
+ + Save result into file
 */
-func main() {
-	revenuCalculator()
-	bank()
-}
 
 func revenuCalculator() {
-	revenue := askAndScanCalculator("Write your Revenue:")
-	expenses := askAndScanCalculator("Write your Expenses:")
-	taxRate := askAndScanCalculator("Write your Tax rate:")
+	revenue := userInputValidation(askAndScanCalculator("Write your Revenue:"))
+	expenses := userInputValidation(askAndScanCalculator("Write your Expenses:"))
+	taxRate := userInputValidation(askAndScanCalculator("Write your Tax rate:"))
 	
 	earningBeforeTax, earningAfterTax, ratio := calculateEbtEatAndRatio(revenue, expenses, taxRate)
+
+	saveDataToFile(earningBeforeTax, earningAfterTax, ratio)
 
 	fmt.Printf("Earning before tax is: %.1f\n", earningBeforeTax)
 	fmt.Printf("Earning after tax is: %.1f\n", earningAfterTax)
 	fmt.Printf("Ratio is: %.1f\n", ratio)
+}
+
+func saveDataToFile(ebt, eat, ratio float64) {
+	data := fmt.Sprintf("%0.2f\n%0.2f\n%0.2f", ebt, eat, ratio)
+	os.WriteFile("profit.txt", []byte(data), 0644)
+}
+
+func userInputValidation(input float64) float64 {
+	if input == 0 {
+		panic("Input should be greater thant 0")
+	} else if input < 0 {
+		panic("Input should be positive")
+	}
+	return input
 }
 
 func askAndScanCalculator(ask string) float64 {
